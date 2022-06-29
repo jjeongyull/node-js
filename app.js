63696a9791f582b8aa5c1a02ca08f416f30b776a
jjeongyull <ckddnjs3698@naver.com>
2022-06-30 01:59:10 +0900

    node js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const helmet = require("helmet");
+const app = express();
+const ejs = require("ejs");
+
+app.set('view engine', 'ejs');
+app.set('views', './views')
+app.use('/public', express.static(__dirname + '/public'));
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded());
+// 사이트 ----> 요청 -- middleware() --> Node.JS
+
+
+const mainRouter = require('./router/mainRouter');
+app.use('/', mainRouter)
+
+app.listen(3000,function(req,res){
+    console.log("서버가 실행되고 있다!")
+})

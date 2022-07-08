3c8622821daa54f9a5aec8a3960f367e8f470f78
jjeongyull <ckddnjs3698@naver.com>
2022-07-09 03:15:01 +0900

    영화후기

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const helmet = require("helmet");
 const app = express();
 const ejs = require("ejs");
+const db = require('./model/db');
 
 app.set('view engine', 'ejs');
 app.set('views', './views')
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use(helmet());
+// app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded());
 // 사이트 ----> 요청 -- middleware() --> Node.JS
@@ -17,5 +18,7 @@ const mainRouter = require('./router/mainRouter');
 app.use('/', mainRouter)
 
 app.listen(3000,function(req,res){
+
+    db.sequelize.sync({force:false})
     console.log("서버가 실행되고 있다!")
 })
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,9 +1,35 @@
 const { query, Router } = require('express');
 const express = require('express');
+const db = require('../model/db');
 const router = express.Router();
 
+
 router.get("/", function(req,res){
-    res.render('index',{title:"EJS 메인페이지"})
+    res.render('index',{title:"영화 리뷰 사이트"})
+})
+
+router.post("/review/create", function(req,res){
+    let movie_id = req.body.movie_id;
+    let review = req.body.review;
+
+    if(movie_id == '' || movie_id == 0){
+        res.send({success:400})
+    }else{
+        db.reviews.create({
+            movie_id:movie_id,
+            review:review
+        }).then(function(result){
+            res.send({success:200})
+        })
+    }
+})
+
+router.get("/review/read", function(req,res){
+    let movie_id = req.query.movie_id;
+
+    db.reviews.findAll({where:{movie_id:movie_id}}).then(function(result){
+        res.send({success:200, data:result})
+    })
 })
 
 router.get("/about", function(req,res){
@@ -16,4 +42,41 @@ router.post("/postapi", function(req,res){
     res.send('POST API')
 })
 
+/*
+   C : Create
+   R : Read
+   U : Update
+   D : Delete
+
+*/
+
+router.get("/data/create",function(req,res){
+    let user_id = parseInt(Math.random() * 10000);
+    db.users.create({user_id:user_id}).then(function(result){
+        res.send({success:200})
+    })
+})
+
+router.get("/data/read", function(req,res){
+    db.users.findAll().then(function(result){
+        res.send({success:200, data:result})
+    })
+})
+
+router.post("/data/update", function(req,res){
+    let target_id = req.body.target_id;
+    db.users.update({user_id:9999},{where:{user_id:target_id}}).then(function(result){
+        res.send({success:200})
+    })
+})
+
+router.post("/data/delete", function(req,res){
+    let target_id = req.body.target_id;
+    db.users.destroy({where:{user_id:target_id}}).then(function(result){
+        res.send({success:200})
+    })
+})
+
+
 module.exports = router
+
